818fcef718845d80e1fb7d23f43dc812c766e852
wissio <wissio178@gmail.com>
2024-05-11 13:32:48 +0300

    first-iteration upd

--- main.go
+++ main.go
@@ -99,7 +99,14 @@ func (s ParcelService) Delete(number int) error {
 func main() {
 	// настройте подключение к БД
 
-	store := // создайте объект ParcelStore функцией NewParcelStore
+	db, err := sql.Open("sqlite", "tracker.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
+
+	store := NewParcelStore(db)
 	service := NewParcelService(store)
 
 	// регистрация посылки
